f6d5ab708277a0b74571febee2b339131e055135
dorsabag123 <dorsabag93@gmail.com>
2023-10-01 23:37:04 +0300

    Update EnemySpawner.cs
    updating postion

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -6,7 +6,16 @@ public class EnemySpawner : MonoBehaviour {
 
     [SerializeField] private GameObject swarmerEnemy;
 
-    private float interval = 6.5f;
+    [SerializeField] private float interval = 6.5f;
+
+    Rigidbody2D rb;
+    Vector2 position;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        position = gameObject.transform.position;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +26,7 @@ public class EnemySpawner : MonoBehaviour {
     private IEnumerator spawnEnemy(float interval, GameObject enemy) 
     { 
         yield return new WaitForSeconds(interval);
-        GameObject newEnemy = Instantiate(enemy, new Vector3(5,5),Quaternion.identity);
+        GameObject newEnemy = Instantiate(enemy, position, Quaternion.identity);
         StartCoroutine(spawnEnemy(interval, enemy));
     }
 }
