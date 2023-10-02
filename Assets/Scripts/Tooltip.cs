bffcdcd2ed2b554f62abefeaa9096a3a02b5a72d
YaelGalili <yael10galili@gmail.com>
2023-10-03 00:35:15 +0300

    tooltip

---
+++ Assets/Scripts/Tooltip.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public string message;
+    //=======================================For GameObjects==================
+    /*private void OnMouseEnter()
+    {
+        TooltipManager._instance.ShowTooltip(message);
+    }
+
+    private void OnMouseExit()
+    {
+        TooltipManager._instance.HideTooltip();
+    }*/
+    //=======================================For Buttons=========================
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        TooltipManager._instance.ShowTooltip(message);
+    }
+
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        TooltipManager._instance.HideTooltip();
+    }
+}
+
+//public class Tooltip : MonoBehaviour
+//{
+//    public string message;
+
+//    private void OnMouseEnter()
+//    {
+//        TooltipManager._instance.ShowTooltip(message);
+//    }
+
+//    private void OnMouseExit()
+//    {
+//        TooltipManager._instance.HideTooltip();
+//    }
+//}
---
+++ Assets/Scripts/TooltipManager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TooltipManager : MonoBehaviour
+{
+    public static TooltipManager _instance;
+    [SerializeField] private Text tooltipText;
+    private RectTransform backgroundRectTransform;
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+            backgroundRectTransform = GetComponent<RectTransform>();
+        }
+    }
+
+    private void Start()
+    {
+        Cursor.visible = true;
+        gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void ShowTooltip(string tooltipStr)
+    {
+        gameObject.SetActive(true);
+        tooltipText.text = tooltipStr;
+        float textPaddingSize = 4f * 2f;
+        Vector2 backgoundSize = new Vector2(tooltipText.preferredWidth + textPaddingSize, tooltipText.preferredHeight + textPaddingSize);
+        backgroundRectTransform.sizeDelta = backgoundSize;
+    }
+
+    public void HideTooltip()
+    {
+        gameObject.SetActive(false);
+        tooltipText.text = string.Empty;
+    }
+}
