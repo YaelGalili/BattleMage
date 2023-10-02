d4f54d8f99ff044398e5fcc5e5b3097c37f1ba17
YaelGalili <yael10galili@gmail.com>
2023-10-02 03:08:17 +0300

    traps prefabs

--- Assets/Scripts/Enemies/Trap.cs
+++ Assets/Scripts/Enemies/Trap.cs
@@ -2,40 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Attack : MonoBehaviour
+public class Trap : MonoBehaviour
 {
-    Collider2D attackCollider;
+    [SerializeField] private float activeTime;
+    private Animator animator;
     public int attackDamage = 10;
     public Vector2 knockback = Vector2.zero;
 
-    private void Awake() {
-        attackCollider = GetComponent<Collider2D>();
-    }
-
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
-
-    protected void OnTriggerEnter2D(Collider2D collision) {
-        // check if can be damaged
         Damageable damageable = collision.GetComponent<Damageable>();
 
-        if (damageable != null) {
-
+        if (damageable != null)
+        {
             Vector2 deliveredKnockback = transform.localScale.x > 0 ? knockback : new Vector2(-knockback.x, knockback.y);
-
-            // damage the target
             bool gotHit = damageable.Hit(attackDamage, deliveredKnockback);
             if (gotHit)
+            {
                 Debug.Log(collision.name + " hit for " + attackDamage);
+            }
         }
+
+        StartCoroutine(ActivateTrap());
+    }
+    private IEnumerator ActivateTrap()
+    {
+        animator.SetBool("triggered", true);
+        yield return new WaitForSeconds(activeTime);
+        animator.SetBool("triggered", false);
     }
 }
